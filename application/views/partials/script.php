837f500476d082044efb42dc6809c4c7309be9c2
juan <juancesarandrianto@gmail.com>
2019-02-05 12:10:19 +0700

    commit
    kelar layoutting

---
+++ application/views/partials/script.php
@@ -0,0 +1,15 @@
+<!-- plugins:js -->
+<script src="<?php echo base_url('vendors/js/vendor.bundle.base.js')?>"></script>
+  <script src="<?php echo base_url('vendors/js/vendor.bundle.addons.js')?>"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="<?php echo base_url('js/off-canvas.js')?>"></script>
+  <script src="<?php echo base_url('js/hoverable-collapse.js')?>"></script>
+  <script src="<?php echo base_url('js/template.js')?>"></script>
+  <script src="<?php echo base_url('js/settings.js')?>"></script>
+  <script src="<?php echo base_url('js/todolist.js')?>"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="<?php echo base_url('js/dashboard.js')?>"></script>
